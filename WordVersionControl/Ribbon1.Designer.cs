6c30d518634123f0feedc557e1edef322ee0ccec
ryan <ryan@974481066@qq.com>
2025-10-16 17:59:33 +0800

    添加项目文件。

---
+++ WordVersionControl/Ribbon1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WordVersionControl
+{
+	partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		public Ribbon1()
+			: base(Globals.Factory.GetRibbonFactory())
+		{
+			InitializeComponent();
+		}
+
+		/// <summary> 
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region 组件设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tab1 = this.Factory.CreateRibbonTab();
+			this.group1 = this.Factory.CreateRibbonGroup();
+			this.btnSaveCommit = this.Factory.CreateRibbonButton();
+			this.btnShowLog = this.Factory.CreateRibbonButton();
+			this.btnCompare = this.Factory.CreateRibbonButton();
+			this.tab1.SuspendLayout();
+			this.group1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tab1
+			// 
+			this.tab1.Groups.Add(this.group1);
+			this.tab1.Label = "版本控制";
+			this.tab1.Name = "tab1";
+			// 
+			// group1
+			// 
+			this.group1.Items.Add(this.btnSaveCommit);
+			this.group1.Items.Add(this.btnShowLog);
+			this.group1.Items.Add(this.btnCompare);
+			this.group1.Label = "操作";
+			this.group1.Name = "group1";
+			// 
+			// btnSaveCommit
+			// 
+			this.btnSaveCommit.Label = "保存并提交";
+			this.btnSaveCommit.Name = "btnSaveCommit";
+			this.btnSaveCommit.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.BtnSaveCommit_Click);
+			// 
+			// btnShowLog
+			// 
+			this.btnShowLog.Label = "查看历史";
+			this.btnShowLog.Name = "btnShowLog";
+			this.btnShowLog.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.BtnShowLog_Click);
+			// 
+			// btnCompare
+			// 
+			this.btnCompare.Label = "比较最近两次";
+			this.btnCompare.Name = "btnCompare";
+			this.btnCompare.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.BtnCompare_Click);
+			// 
+			// Ribbon1
+			// 
+			this.Name = "Ribbon1";
+			this.RibbonType = "Microsoft.Word.Document";
+			this.Tabs.Add(this.tab1);
+			this.tab1.ResumeLayout(false);
+			this.tab1.PerformLayout();
+			this.group1.ResumeLayout(false);
+			this.group1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+		private Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+		private Microsoft.Office.Tools.Ribbon.RibbonButton btnSaveCommit;
+		private Microsoft.Office.Tools.Ribbon.RibbonButton btnShowLog;
+		private Microsoft.Office.Tools.Ribbon.RibbonButton btnCompare;
+	}
+
+	partial class ThisRibbonCollection
+	{
+		internal Ribbon1 Ribbon1
+		{
+			get { return this.GetRibbon<Ribbon1>(); }
+		}
+	}
+}
---
+++ WordVersionControl/Ribbon1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace WordVersionControl
+{
+	public partial class Ribbon1
+	{
+		private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+		{
+			
+		}
+
+		private void BtnSaveCommit_Click(object sender, RibbonControlEventArgs e)
+		{
+			var doc = Globals.ThisAddIn.Application.ActiveDocument;
+			if (doc == null) { MessageBox.Show("未打开任何文档"); return; }
+
+			string docPath = doc.FullName;
+			string dir = Path.GetDirectoryName(docPath);
+			doc.Save();
+
+			EnsureGitRepo(dir);
+			RunGit(dir, $"add \"{Path.GetFileName(docPath)}\"");
+			RunGit(dir, $"commit -m \"Auto save {DateTime.Now:yyyy-MM-dd HH:mm:ss}\"");
+			MessageBox.Show("已提交到 Git");
+		}
+
+		private void BtnShowLog_Click(object sender, RibbonControlEventArgs e)
+		{
+			var doc = Globals.ThisAddIn.Application.ActiveDocument;
+			if (doc == null) { MessageBox.Show("未打开任何文档"); return; }
+			string dir = Path.GetDirectoryName(doc.FullName);
+
+			string output = RunGit(dir, "log --oneline -n 10");
+			MessageBox.Show(output, "最近版本历史");
+		}
+
+		private void BtnCompare_Click(object sender, RibbonControlEventArgs e)
+		{
+			var doc = Globals.ThisAddIn.Application.ActiveDocument;
+			if (doc == null) { MessageBox.Show("未打开任何文档"); return; }
+			string dir = Path.GetDirectoryName(doc.FullName);
+			string fileName = Path.GetFileName(doc.FullName);
+
+			// 提取最近两次提交的版本
+			string oldPath = Path.Combine(dir, "_old.docx");
+			string newPath = Path.Combine(dir, "_new.docx");
+			ExportGitVersion(dir, $"HEAD~1:\"{fileName}\"", oldPath);
+			ExportGitVersion(dir, $"HEAD:\"{fileName}\"", newPath);
+
+			// 调用 Word 的比较功能
+			var app = Globals.ThisAddIn.Application;
+			app.CompareDocuments(
+				app.Documents.Open(oldPath, ReadOnly: true),
+				app.Documents.Open(newPath, ReadOnly: true),
+				Microsoft.Office.Interop.Word.WdCompareDestination.wdCompareDestinationNew
+			);
+		}
+
+		private void EnsureGitRepo(string dir)
+		{
+			if (!Directory.Exists(Path.Combine(dir, ".git")))
+				RunGit(dir, "init");
+		}
+
+		private string RunGit(string dir, string args)
+		{
+			var psi = new ProcessStartInfo("git", args)
+			{
+				WorkingDirectory = dir,
+				RedirectStandardOutput = true,
+				RedirectStandardError = true,
+				UseShellExecute = false,
+				CreateNoWindow = true,
+				StandardOutputEncoding = Encoding.UTF8,
+				StandardErrorEncoding = Encoding.UTF8
+			};
+			using (var proc = Process.Start(psi))
+			{
+				string output = proc.StandardOutput.ReadToEnd() + proc.StandardError.ReadToEnd();
+				proc.WaitForExit();
+				return output;
+			}
+		}
+
+		private void ExportGitVersion(string dir, string gitSpec, string outputPath)
+		{
+			var psi = new ProcessStartInfo("git", $"show {gitSpec}")
+			{
+				WorkingDirectory = dir,
+				RedirectStandardOutput = true,
+				RedirectStandardError = true,
+				UseShellExecute = false,
+				CreateNoWindow = true,
+				StandardOutputEncoding = Encoding.UTF8
+			};
+
+			using (var proc = Process.Start(psi))
+			{
+				using (var fs = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+				{
+					proc.StandardOutput.BaseStream.CopyTo(fs);
+				}
+				proc.WaitForExit();
+
+				if (proc.ExitCode != 0)
+				{
+					string err = proc.StandardError.ReadToEnd();
+					throw new Exception($"Git show 失败: {err}");
+				}
+			}
+		}
+	}
+}
